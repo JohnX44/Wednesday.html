8c2e700a33bb9c6a0bf3b7e991ebab53711b6ceb
JohnX44 <130286369+JohnX44@users.noreply.github.com>
2024-08-31 15:59:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function convertDogAge() {
+    const dogAge = document.getElementById('dogAge').value;
+    let humanAge;
+
+    if (dogAge <= 2) {
+        humanAge = dogAge * 10.5;
+    } else {
+        humanAge = (2 * 10.5) + ((dogAge - 2) * 4);
+    }
+
+    document.getElementById('result').textContent = `Your dog's age in human years is approximately: ${humanAge} years.`;
+}
+document.addEventListener('DOMContentLoaded', (event) => {
+    const audio = document.getElementById('background-music');
+    audio.muted = false;
+});
